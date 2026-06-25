1ec9b6db50b83a112ad261fb4559b87e841dcdcf
santu8597 <162991657+santu8597@users.noreply.github.com>
2026-06-25 11:40:15 +0530

    test 2

--- services_aiori_v2/celery.py
+++ services_aiori_v2/celery.py
@@ -6,3 +6,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services_aiori_v2.settings')
 app = Celery('services_aiori_v2')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
